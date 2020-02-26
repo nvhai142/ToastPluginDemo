9ec5d42adce32e8adfa47ec2c3df9d6cf182356c
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-02-26 16:54:29 +0700

    Update AppDelegate+notification.m

--- src/ios/AppDelegate+notification.m
+++ src/ios/AppDelegate+notification.m
@@ -70,7 +70,7 @@ NSString *const pushPluginApplicationDidBecomeActiveNotification = @"pushPluginA
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
    // PushPlugin *pushHandler = [self getCommandInstance:@"PushNotification"];
   //  [pushHandler didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
-  [[UIApplication sharedApplication] setApplicationIconBadgeNumber:5];
+  [[UIApplication sharedApplication] unregisterForRemoteNotifications];
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
